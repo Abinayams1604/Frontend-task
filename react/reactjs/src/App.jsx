7f8860bd7d9b662089e408dffb293ac1ec5e9cb5
Abinayams1604 <abinayams1604@gmail.com>
2026-03-11 19:48:25 +0530

    First commit

---
+++ react/reactjs/src/App.jsx
@@ -0,0 +1,19 @@
+import react from 'react';
+import Home from './Component/Home';
+import Counter from './Component/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React day_1</h1>
+    <Home/>
+  
+     <Counter/>
+
+
+    </div>
+  );
+}
+
+export default App;
+
